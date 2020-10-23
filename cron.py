27be0f71686fcf80d111f4932c6a5c674a8a946b
Tim <tima.titarenko.001@gmail.com>
2020-10-23 11:13:22 +0300

    Add project

--- cron.py
+++ cron.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import time
 from crontab import CronTab
 from croniter import croniter
+import logging
 
 
 def child(command):
-    #print(command)
     os.system(command)
     os._exit(0)
 
@@ -14,6 +14,11 @@ def child(command):
 def parent():
     base = datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,
                     datetime.now().minute)
+
+    logging.basicConfig(filename='logi.log', filemode='w', level=logging.DEBUG,
+                        format='%(asctime)s:%(name)s:%(levelname)s: %(message)s',
+                        )
+
     word_list=[]
     time_list = []
     iter_list = []
@@ -28,10 +33,6 @@ def parent():
     for i in range(len(word_list)):
         count=0
         for j in word_list[i]:
-
-            # if j == '@':
-            #     count = len(word_list[i].split()[0])
-            #     print(len(word_list[i].split()[0]))
             if j.isalpha():
                 if word_list[i][count-1:count] == '/':
                     count-=1
@@ -47,21 +48,21 @@ def parent():
 
 
     size = len(word_list)
-    print(word_list)
-    print(time_list)
-    print(counters)
-    print(datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,
-                   datetime.now().minute, datetime.now().second))
     while True:
 
         i=0
         for i in range(size):
             if time_list[i] == datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,datetime.now().minute):
                 time_list[i] = iter_list[i].get_next(datetime)
-                print(datetime.now())
+                logging.info("Starting process")
                 pid = os.fork()
                 if pid == 0:
-                    child(word_list[i][counters[i]:])
+
+                     try:
+                         child(word_list[i][counters[i]:])
+                     except Exception as e:
+                         logging.error(e)
+                logging.info("Finish process")
                
         time.sleep(1)
 
--- print.py
+++ print.py
@@ -11,3 +11,66 @@ if __name__ == '__main__':
 	main()
 
 
+# def child(command):
+#    # print(command)
+#     os.system(command)
+#     os._exit(0)
+#
+#
+# def parent():
+#     base = datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,
+#                     datetime.now().minute)
+#     word_list=[]
+#     time_list = []
+#     iter_list = []
+#     cron = CronTab(tabfile='task.tab')
+#     for word in cron:
+#         print(word)
+#         str_word =str(word)
+#         match = re.match("^[/d]")
+#         if str_word[:1].:
+#             continue
+#         word_list.append(str_word)
+#     print(word_list)
+#     for i in range(len(word_list)):
+#         #print(word_list[i][:10])
+#         iter = croniter(word_list[i][:10], base)
+#         iter_list.append(iter)
+#
+#     for j in range(len(word_list)):
+#         compare_time = iter_list[j].get_next(datetime)
+#         time_list.append(compare_time)
+#
+#     #print(time_list)
+#     size = len(word_list)
+#
+#     while True:
+#         print(datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,
+#                        datetime.now().minute, datetime.now().second))
+#         i=0
+#         for i in range(size):
+#             if time_list[i] == datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour,datetime.now().minute):
+#                 time_list[i] = iter_list[i].get_next(datetime)
+#                 pid = os.fork()
+#                 if pid == 0:
+#                     child(word_list[i][10:])
+#                 #os.system(word_list[i][10:])
+#
+#                     # child(word_list[i][10:])
+#         time.sleep(1)
+#
+#
+# parent()
+
+# base =datetime(datetime.now().year,datetime.now().month,datetime.now().day,datetime.now().hour,datetime.now().minute)
+# iter = croniter(rec_time,base)
+# while True:
+#     m_time = iter.get_next(datetime)
+#     print("NEXT ", m_time)
+#     while True:
+#         print(datetime(datetime.now().year,datetime.now().month,datetime.now().day,datetime.now().hour,datetime.now().minute,datetime.now().second))
+#         if m_time == datetime(datetime.now().year,datetime.now().month,datetime.now().day,datetime.now().hour,datetime.now().minute):
+#             os.system(command)
+#             break
+#         time.sleep(1)
+# os.exit(0)
